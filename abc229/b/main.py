ddc2abc4d92d427e495c582819b67a4518ce0dd3
yunagi <aquayutoz[icloud.com>
2022-04-17 22:43:51 +0900

    abc229b

--- abc229/b/main.py
+++ abc229/b/main.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+i,j = map(str,input().split())
+n = len(i)
+z = len(j)
+while True:
+    if n == 0 or z == 0:
+        print("Easy")
+        break
+    if len(str(int(i[n-1:n])+int(j[z-1:z]))) == 2:
+        print("Hard")
+        break
+    else:
+        n -= 1
+        z -= 1
