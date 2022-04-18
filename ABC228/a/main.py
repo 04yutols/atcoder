544161e93f34a64b2eae7cccf5941fda46ddd39e
yunagi <aquayutoz[icloud.com>
2022-04-18 23:30:07 +0900

    abc228ab

--- ABC228/a/main.py
+++ ABC228/a/main.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+A,B,C = map(int,input().split())
+if A > B:
+    B = B+24
+    if A > C:
+        C += 24
+if A <= C and B > C:
+    print("Yes")
+else:
+    print("No")
--- ABC228/b/main.py
+++ ABC228/b/main.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+N,X = map(int,input().split())
+A = list(map(int,input().split()))
+flag = []
+for i in range(N):
+    flag.append(False)
+flag[X-1]=True
+st = A[X-1]
+while True:
+    if flag[st-1] == True:
+        break
+    else:
+        flag[st-1] = True
+        st = A[st-1]
+print(flag.count(True))
