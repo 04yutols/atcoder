f76fe7add9fe58c76343c5b4a5ce961f7c78a303
yunagi <aquayutoz[icloud.com>
2022-04-19 23:32:18 +0900

    abc228 c

--- ABC228/c/main.py
+++ ABC228/c/main.py
@@ -1,1 +1,35 @@
 #!/usr/bin/env python3
+from itertools import count
+from operator import itemgetter
+from textwrap import indent
+import copy
+N,K = map(int,input().split())
+P = [list(map(int, input().split())) for _ in range(N)]
+oP = copy.copy(P)
+ans = []
+count = 0
+while True:
+    ans.append("No")
+    count += 1
+    if count > N-1:
+        count = 0
+        break
+P.sort(reverse=True,key=lambda x:x[0]+x[1]+x[2])
+border = sum(P[K])-300
+while True:
+    if count > N-1:
+        count = 0
+        break
+    if sum(P[count])>=border:
+        ans[oP.index(P[count])] = "Yes"
+        count += 1
+    else:
+        count =0
+        break
+    
+while True:
+    if count > N-1:
+        break
+    print(ans[count])
+    count += 1
+    
